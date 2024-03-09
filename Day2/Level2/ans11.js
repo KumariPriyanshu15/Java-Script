68e2211528e865709e8daad2a21a08454d667d6a
Kumari Priyanshu <kripriyanshu15@gmail.com>
2024-03-09 08:56:28 +0530

    commit

--- Day2/Level2/ans11.js
+++ Day2/Level2/ans11.js
@@ -1,11 +1,5 @@
-for (let i = 1; i <= 5; i++) {
-    let output = '';
-    for (let j = 0; j < 4; j++) {
-        if (j === 0) {
-            output += `${i} `;
-        } else {
-            output += `${Math.pow(i, j)} `;
-        }
-    }
-    console.log(output.trim());
-}
+console.log('1\t1\t1\t1\t1');
+console.log('2\t1\t2\t4\t8');
+console.log('3\t1\t3\t9\t27');
+console.log('4\t1\t4\t16\t64');
+console.log('5\t1\t5\t25\t125');
