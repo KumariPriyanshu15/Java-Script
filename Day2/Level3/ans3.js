f9dc07ecfa8e2154706d886ec38c1966239b4c88
Kumari Priyanshu <kripriyanshu15@gmail.com>
2024-03-09 21:05:00 +0530

    commit

--- Day2/Level3/ans3.js
+++ Day2/Level3/ans3.js
@@ -1,2 +1,3 @@
-const word = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
-console.log(word.replace(/[^a-zA-Z0-9]/g," "));
+const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+console.log(sentence.replace(/[^a-zA-Z0-9]/g," "));
+console.log(sentence.match(/\d+/g))
