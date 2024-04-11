08c3385e96f95f1d7c73707f8a27a2a0b4f2a1ed
Kumari Priyanshu <kripriyanshu15@gmail.com>
2024-04-11 21:24:51 +0530

    commit

--- Day4/Level2/ans2.js
+++ Day4/Level2/ans2.js
@@ -1,32 +1,38 @@
-// let dayUserInput = prompt('month') 
-// let season = dayUserInput.toLowerCase()
-//     switch (season) {
-//         case 'September':
-//         case 'October':
-//         case 'November':
-//             console.log('Autumn') 
-//             break
-            
-//         case 'December':
-//         case 'January':
-//         case 'February':
-//             console.log('Winter') 
-//             break
-            
-//         case 'March':
-//         case 'April':
-//         case 'May':
-//             console.log('Spring') 
-//             break
-            
-            
-//         case 'June':
-//         case 'July':
-//         case 'August':
-//             console.log('Summer') 
-//             break
-           
-//         default:
-//             console.log('Invalid Option') 
-//     }
-    
+// const season = prompt('enter the month:')
+
+//  if (season == 'september') {
+//  console.log("The Season is Autumn.")
+//  }
+//  else if (season == 'october') {
+//  console.log("The Season is Autumn.")
+//  }
+//  else if (season == 'november') {
+//  console.log("The Season is Autumn.")
+//  }
+//  else if (season == 'december') {
+//  console.log("The Season is Winter.")
+//  }
+//  else if (season == 'january') {
+//  console.log("The Season is Winter.")
+//  }
+//  else if (season == 'february') {
+//  console.log("The Season is Winter.")
+//  }
+//  else if (season == 'march') {
+//  console.log("The Season is Winter.")
+//  }
+//  else if (season == 'april') {
+//  console.log("The Season is Spring.")
+//  }
+//  else if (season == 'may') {
+//  console.log("The Season is Spring.")
+//  }
+//  else if (season == 'june') {
+//  console.log("The Season is Summer.")
+//  }
+//  else if (season == 'july') {
+//  console.log("The Season is Summer.")
+//  }
+//  else if (season == 'august') {
+//  console.log("The Season is Summer.")
+//  }
---
+++ Day5/Level1/ans5.js
@@ -0,0 +1,40 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ];
+  
+  const mixedDataTypes = [
+    'John Doe',     
+    25,             
+    true,           
+    {                
+      city: 'New York',
+      country: 'USA'
+    },
+    ['apple', 'banana', 'orange'],  
+    function() {    
+      return 'This is a function';
+    }
+  ];
+  
+  console.log("Length of mixedDataTypes array:", mixedDataTypes.length);
+  
